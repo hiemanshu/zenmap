4bf178c27165799e18e7f49e3f910aa935dc085f
Hiemanshu Sharma <hiemanshu@gmail.com>
2011-03-25 00:41:27 +0530

    Console output works now

--- zenmap.py
+++ zenmap.py
@@ -2,7 +2,13 @@
 
 import gui
 from PyQt4 import QtCore, QtGui
-import sys
+import sys, os, time
+import subprocess
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
 
 class Zenmap(QtGui.QMainWindow):
     
@@ -10,12 +16,24 @@ class Zenmap(QtGui.QMainWindow):
         QtGui.QWidget.__init__(self, parent)
         self.ui = gui.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.timer = QtCore.QTimer()
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL('timeout()'), self.update)
         
     def changeCommand(self):
 	print "changeCommand()"
 	
     def startScan(self):
-	print "Start Scan"
+	self.ui.textBrowser.clear()
+	self.process = subprocess.Popen(['nmap', '-T4', '-A', '-v', '192.168.1.*'], shell = False, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+	self.timer.start(5)
+	
+    def update(self):
+	if self.process.stdout.readline() == '' and self.process.poll() != None:
+	    self.timer.stop()
+	else:
+	    self.ui.textBrowser.insertPlainText(self.process.stdout.readline())
+	self.process.stdout.flush()
+	
 
     def stopScan(self):
 	print "Stop Scan"
